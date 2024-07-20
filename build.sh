382e8844fa6ce55b4aa38a80ab08443e71fb976f
ir1n1o57 <c0nc0rde1o57@gmail.com>
2024-07-20 22:11:48 +0900

    Who knows if it would work or not

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install and remove rpm packages
-rpm-ostree install android-tools gcc gcc-c++ gdb python3 python3-pip \
+dnf install android-tools gcc gcc-c++ gdb python3 python3-pip \
 					cowsay figlet lolcat neofetch \
 					netcat nmap testdisk vim-enhanced youtube-dl whois wl-clipboard \
 					lm_sensors vulkan-tools \
       				adw-gtk3-theme papirus-icon-theme ibus-mozc gnome-console
 
-rpm-ostree override remove firefox firefox-langpacks \
+dnf autoremove firefox firefox-langpacks \
 							gnome-software gnome-software-rpm-ostree epiphany-runtime gnome-terminal gnome-terminal-nautilus \
 							anthy-unicode ibus-anthy ibus-anthy-python kasumi-common kasumi-unicode ibus-hangul libhangul ibus-libzhuyin libzhuyin \
 							nano nano-default-editor default-editor 
