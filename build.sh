724eca7cf443ada02e4d5e33ffc3c99e58342045
ir1n1o57 <c0nc0rde1o57@gmail.com>
2024-07-20 21:07:56 +0900

    Fixed a typo

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install and remove rpm packages
-rpm-ostree install android-tools cowsay figlet gcc gcc-c++ gdb lolcat neofetch netcat nmap python3 python3-pip testdisk vim-enhanced whois wl-clipboard adw-gtk3-theme youtube-dl lm-sensors vulkan-tools
+rpm-ostree install android-tools cowsay figlet gcc gcc-c++ gdb lolcat neofetch netcat nmap python3 python3-pip testdisk vim-enhanced whois wl-clipboard adw-gtk3-theme youtube-dl lm_sensors vulkan-tools
 rpm-ostree override remove firefox firefox-langpacks gnome-software-rpm-ostree
 
 # Install flatpak packages [WIP]
