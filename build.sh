61a0b809a8d50eeb8e93c02c60f50fe547e736ea
ir1n1o57 <c0nc0rde1o57@gmail.com>
2024-07-20 21:03:24 +0900

    Added some packages to install

--- build.sh
+++ build.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
 set -ouex pipefail
-
 RELEASE="$(rpm -E %fedora)"
 
+# Install and remove rpm packages
+rpm-ostree install android-tools cowsay figlet gcc gcc-c++ gdb lolcat neofetch netcat nmap python3 python3-pip testdisk vim-enhanced whois wl-clipboard adw-gtk3-theme youtube-dl lm-sensors vulkan-tools
+rpm-ostree override remove firefox firefox-langpacks gnome-software-rpm-ostree
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Install flatpak packages [WIP]
