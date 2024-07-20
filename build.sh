0593247c1e55881647c2e24d3e4352f166c10c05
ir1n1o57 <c0nc0rde1o57@gmail.com>
2024-07-20 21:59:28 +0900

    Fixed an error

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ RELEASE="$(rpm -E %fedora)"
 # Install and remove rpm packages
 rpm-ostree install android-tools gcc gcc-c++ gdb python3 python3-pip \
 					cowsay figlet lolcat neofetch \
-					netcat nmap testdisk vim-enhanced vim-default-editor youtube-dl whois wl-clipboard \
+					netcat nmap testdisk vim-enhanced youtube-dl whois wl-clipboard \
 					lm_sensors vulkan-tools \
       				adw-gtk3-theme papirus-icon-theme ibus-mozc gnome-console
 
 rpm-ostree override remove firefox firefox-langpacks \
 							gnome-software gnome-software-rpm-ostree epiphany-runtime gnome-terminal gnome-terminal-nautilus \
 							anthy-unicode ibus-anthy ibus-anthy-python kasumi-common kasumi-unicode ibus-hangul libhangul ibus-libzhuyin libzhuyin \
-							nano nano-default-editor 
+							nano nano-default-editor default-editor 
 							
 
 # Install flatpak packages [WIP]
