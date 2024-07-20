b034c0f0fdde13cd61ce1e57c541c56da721377e
ir1n1o57 <c0nc0rde1o57@gmail.com>
2024-07-20 22:22:20 +0900

    Attempting to debloat

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install and remove rpm packages
-dnf install android-tools gcc gcc-c++ gdb python3 python3-pip \
+echo "install_weak_deps=False" >> /etc/dnf/dnf.conf
+
+rpm-ostree install android-tools gcc gcc-c++ gdb python3 python3-pip \
 					cowsay figlet lolcat neofetch \
 					netcat nmap testdisk vim-enhanced youtube-dl whois wl-clipboard \
 					lm_sensors vulkan-tools \
       				adw-gtk3-theme papirus-icon-theme ibus-mozc gnome-console
 
-dnf autoremove firefox firefox-langpacks \
+rpm-ostree override remove firefox firefox-langpacks \
 							gnome-software gnome-software-rpm-ostree epiphany-runtime gnome-terminal gnome-terminal-nautilus \
 							anthy-unicode ibus-anthy ibus-anthy-python kasumi-common kasumi-unicode ibus-hangul libhangul ibus-libzhuyin libzhuyin \
 							nano nano-default-editor default-editor 
